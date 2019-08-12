735c7d28d56fe084fe7024f39ca9af969cefab50
33581 <aluno@etecaf.local>
2019-08-12 17:53:09 -0300

    primeiro commit

---
+++ Pokemon_33581/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Pokemon_33581
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPokemonA = new System.Windows.Forms.Label();
+            this.lblPokemonB = new System.Windows.Forms.Label();
+            this.txtAtaque = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblPoder = new System.Windows.Forms.Label();
+            this.lblDefesa = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.txtPoder = new System.Windows.Forms.TextBox();
+            this.txtDefesa = new System.Windows.Forms.TextBox();
+            this.lblDefesaB = new System.Windows.Forms.Label();
+            this.lblPoderB = new System.Windows.Forms.Label();
+            this.lblLevelB = new System.Windows.Forms.Label();
+            this.lblAtaqueB = new System.Windows.Forms.Label();
+            this.lblLevelQuant = new System.Windows.Forms.Label();
+            this.lblPoderQuant = new System.Windows.Forms.Label();
+            this.lblDefesaQuant = new System.Windows.Forms.Label();
+            this.lblAtqueQuant = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.bntResultado = new System.Windows.Forms.Button();
+            this.bntFechar = new System.Windows.Forms.Button();
+            this.bntLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPokemonA
+            // 
+            this.lblPokemonA.AutoSize = true;
+            this.lblPokemonA.Location = new System.Drawing.Point(64, 34);
+            this.lblPokemonA.Name = "lblPokemonA";
+            this.lblPokemonA.Size = new System.Drawing.Size(65, 13);
+            this.lblPokemonA.TabIndex = 0;
+            this.lblPokemonA.Text = "Pokemon A:";
+            // 
+            // lblPokemonB
+            // 
+            this.lblPokemonB.AutoSize = true;
+            this.lblPokemonB.Location = new System.Drawing.Point(249, 34);
+            this.lblPokemonB.Name = "lblPokemonB";
+            this.lblPokemonB.Size = new System.Drawing.Size(65, 13);
+            this.lblPokemonB.TabIndex = 1;
+            this.lblPokemonB.Text = "Pokemon B:";
+            this.lblPokemonB.Click += new System.EventHandler(this.lblPokemonB_Click);
+            // 
+            // txtAtaque
+            // 
+            this.txtAtaque.Location = new System.Drawing.Point(104, 155);
+            this.txtAtaque.Name = "txtAtaque";
+            this.txtAtaque.Size = new System.Drawing.Size(35, 20);
+            this.txtAtaque.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ataque:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(217, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(64, 65);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(36, 13);
+            this.lblLevel.TabIndex = 5;
+            this.lblLevel.Text = "Level:";
+            // 
+            // lblPoder
+            // 
+            this.lblPoder.AutoSize = true;
+            this.lblPoder.Location = new System.Drawing.Point(64, 93);
+            this.lblPoder.Name = "lblPoder";
+            this.lblPoder.Size = new System.Drawing.Size(38, 13);
+            this.lblPoder.TabIndex = 6;
+            this.lblPoder.Text = "Poder:";
+            this.lblPoder.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblDefesa
+            // 
+            this.lblDefesa.AutoSize = true;
+            this.lblDefesa.Location = new System.Drawing.Point(64, 122);
+            this.lblDefesa.Name = "lblDefesa";
+            this.lblDefesa.Size = new System.Drawing.Size(44, 13);
+            this.lblDefesa.TabIndex = 7;
+            this.lblDefesa.Text = "Defesa:";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(104, 62);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(25, 20);
+            this.txtLevel.TabIndex = 8;
+            // 
+            // txtPoder
+            // 
+            this.txtPoder.Location = new System.Drawing.Point(104, 88);
+            this.txtPoder.Name = "txtPoder";
+            this.txtPoder.Size = new System.Drawing.Size(25, 20);
+            this.txtPoder.TabIndex = 9;
+            // 
+            // txtDefesa
+            // 
+            this.txtDefesa.Location = new System.Drawing.Point(107, 118);
+            this.txtDefesa.Name = "txtDefesa";
+            this.txtDefesa.Size = new System.Drawing.Size(25, 20);
+            this.txtDefesa.TabIndex = 10;
+            // 
+            // lblDefesaB
+            // 
+            this.lblDefesaB.AutoSize = true;
+            this.lblDefesaB.Location = new System.Drawing.Point(249, 119);
+            this.lblDefesaB.Name = "lblDefesaB";
+            this.lblDefesaB.Size = new System.Drawing.Size(44, 13);
+            this.lblDefesaB.TabIndex = 15;
+            this.lblDefesaB.Text = "Defesa:";
+            // 
+            // lblPoderB
+            // 
+            this.lblPoderB.AutoSize = true;
+            this.lblPoderB.Location = new System.Drawing.Point(249, 90);
+            this.lblPoderB.Name = "lblPoderB";
+            this.lblPoderB.Size = new System.Drawing.Size(38, 13);
+            this.lblPoderB.TabIndex = 14;
+            this.lblPoderB.Text = "Poder:";
+            // 
+            // lblLevelB
+            // 
+            this.lblLevelB.AutoSize = true;
+            this.lblLevelB.Location = new System.Drawing.Point(249, 62);
+            this.lblLevelB.Name = "lblLevelB";
+            this.lblLevelB.Size = new System.Drawing.Size(36, 13);
+            this.lblLevelB.TabIndex = 13;
+            this.lblLevelB.Text = "Level:";
+            // 
+            // lblAtaqueB
+            // 
+            this.lblAtaqueB.AutoSize = true;
+            this.lblAtaqueB.Location = new System.Drawing.Point(249, 155);
+            this.lblAtaqueB.Name = "lblAtaqueB";
+            this.lblAtaqueB.Size = new System.Drawing.Size(44, 13);
+            this.lblAtaqueB.TabIndex = 12;
+            this.lblAtaqueB.Text = "Ataque:";
+            // 
+            // lblLevelQuant
+            // 
+            this.lblLevelQuant.AutoSize = true;
+            this.lblLevelQuant.Location = new System.Drawing.Point(291, 62);
+            this.lblLevelQuant.Name = "lblLevelQuant";
+            this.lblLevelQuant.Size = new System.Drawing.Size(19, 13);
+            this.lblLevelQuant.TabIndex = 16;
+            this.lblLevelQuant.Text = "75";
+            // 
+            // lblPoderQuant
+            // 
+            this.lblPoderQuant.AutoSize = true;
+            this.lblPoderQuant.Location = new System.Drawing.Point(293, 90);
+            this.lblPoderQuant.Name = "lblPoderQuant";
+            this.lblPoderQuant.Size = new System.Drawing.Size(19, 13);
+            this.lblPoderQuant.TabIndex = 17;
+            this.lblPoderQuant.Text = "65";
+            // 
+            // lblDefesaQuant
+            // 
+            this.lblDefesaQuant.AutoSize = true;
+            this.lblDefesaQuant.Location = new System.Drawing.Point(295, 121);
+            this.lblDefesaQuant.Name = "lblDefesaQuant";
+            this.lblDefesaQuant.Size = new System.Drawing.Size(25, 13);
+            this.lblDefesaQuant.TabIndex = 18;
+            this.lblDefesaQuant.Text = "163";
+            // 
+            // lblAtqueQuant
+            // 
+            this.lblAtqueQuant.AutoSize = true;
+            this.lblAtqueQuant.Location = new System.Drawing.Point(295, 155);
+            this.lblAtqueQuant.Name = "lblAtqueQuant";
+            this.lblAtqueQuant.Size = new System.Drawing.Size(25, 13);
+            this.lblAtqueQuant.TabIndex = 19;
+            this.lblAtqueQuant.Text = "123";
+            this.lblAtqueQuant.Click += new System.EventHandler(this.lblAtqueQuant_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Danos Total:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(146, 219);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 21;
+            // 
+            // bntResultado
+            // 
+            this.bntResultado.Location = new System.Drawing.Point(159, 263);
+            this.bntResultado.Name = "bntResultado";
+            this.bntResultado.Size = new System.Drawing.Size(75, 23);
+            this.bntResultado.TabIndex = 22;
+            this.bntResultado.Text = "Resultado";
+            this.bntResultado.UseVisualStyleBackColor = true;
+            this.bntResultado.Click += new System.EventHandler(this.bntResultado_Click);
+            // 
+            // bntFechar
+            // 
+            this.bntFechar.Location = new System.Drawing.Point(252, 263);
+            this.bntFechar.Name = "bntFechar";
+            this.bntFechar.Size = new System.Drawing.Size(75, 23);
+            this.bntFechar.TabIndex = 23;
+            this.bntFechar.Text = "Fechar";
+            this.bntFechar.UseVisualStyleBackColor = true;
+            // 
+            // bntLimpar
+            // 
+            this.bntLimpar.Location = new System.Drawing.Point(54, 263);
+            this.bntLimpar.Name = "bntLimpar";
+            this.bntLimpar.Size = new System.Drawing.Size(75, 23);
+            this.bntLimpar.TabIndex = 24;
+            this.bntLimpar.Text = "Limpar";
+            this.bntLimpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 298);
+            this.Controls.Add(this.bntLimpar);
+            this.Controls.Add(this.bntFechar);
+            this.Controls.Add(this.bntResultado);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAtqueQuant);
+            this.Controls.Add(this.lblDefesaQuant);
+            this.Controls.Add(this.lblPoderQuant);
+            this.Controls.Add(this.lblLevelQuant);
+            this.Controls.Add(this.lblDefesaB);
+            this.Controls.Add(this.lblPoderB);
+            this.Controls.Add(this.lblLevelB);
+            this.Controls.Add(this.lblAtaqueB);
+            this.Controls.Add(this.txtDefesa);
+            this.Controls.Add(this.txtPoder);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.lblDefesa);
+            this.Controls.Add(this.lblPoder);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAtaque);
+            this.Controls.Add(this.lblPokemonB);
+            this.Controls.Add(this.lblPokemonA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPokemonA;
+        private System.Windows.Forms.Label lblPokemonB;
+        private System.Windows.Forms.TextBox txtAtaque;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblPoder;
+        private System.Windows.Forms.Label lblDefesa;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.TextBox txtPoder;
+        private System.Windows.Forms.TextBox txtDefesa;
+        private System.Windows.Forms.Label lblDefesaB;
+        private System.Windows.Forms.Label lblPoderB;
+        private System.Windows.Forms.Label lblLevelB;
+        private System.Windows.Forms.Label lblAtaqueB;
+        private System.Windows.Forms.Label lblLevelQuant;
+        private System.Windows.Forms.Label lblPoderQuant;
+        private System.Windows.Forms.Label lblDefesaQuant;
+        private System.Windows.Forms.Label lblAtqueQuant;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button bntResultado;
+        private System.Windows.Forms.Button bntFechar;
+        private System.Windows.Forms.Button bntLimpar;
+    }
+}
+
---
+++ Pokemon_33581/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pokemon_33581
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPokemonB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAtqueQuant_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntResultado_Click(object sender, EventArgs e)
+        {
+            double A, D, L, P;
+
+            try
+            {
+                A = double.Parse(txtAtaque.Text);
+                D = double.Parse(txtDefesa.Text);
+                L = double.Parse(txtLevel.Text);
+                P = double.Parse(txtPoder.Text);
+            }
+            catch (Exception)
+            {
+                Message.Show("Digite Apenas Numeros");
+                return;
+            }
+
+
+        }
+    }
+}
